556667d99509f205525b817b8ee025dc936d97d6
Sulaiman <sulaimain@kobo360.com>
2021-01-01 20:00:45 +0100

    side bar footer

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,9 +1,18 @@
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import React from 'react';
 import './sidebar.css';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarItem from '../SidebarItem/sidebarItem';
 import InboxIcon from '@material-ui/icons/Inbox';
+import StarIcon from '@material-ui/icons/Star';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import NoteIcon from '@material-ui/icons/Note';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import PersonIcon from '@material-ui/icons/Person';
+import DuoIcon from '@material-ui/icons/Duo';
+import PhoneIcon from '@material-ui/icons/Phone';
 
 const Sidebar = () => {
   return (  
@@ -15,6 +24,26 @@ const Sidebar = () => {
         Compose
       </Button>
       <SidebarItem Icon={InboxIcon} title="Inbox" count={54}/>
+      <SidebarItem Icon={StarIcon} title="Starred" count={25} selected={true}/>
+      <SidebarItem Icon={AccessTimeIcon} title="Snoozed" count={25}/>
+      <SidebarItem Icon={LabelImportantIcon} title="Important" count={25}/>
+      <SidebarItem Icon={NearMeIcon} title="Sent" count={25} selected={true}/>
+      <SidebarItem Icon={NoteIcon} title="Drafts" count={15}/>
+      <SidebarItem Icon={ExpandMoreIcon} title="More" count={15}/>
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/SidebarItem/sidebarItem.js
+++ src/components/SidebarItem/sidebarItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './sidebarItem.css';
 
-const SidebarItem = ({ Icon, title, count}) => {
+const SidebarItem = ({ Icon, title, count, selected}) => {
   return (  
-    <div className="sidebarItem">
+    <div className={`sidebarItem ${selected ? 'sidebarItem__active' : ''}`}>
       <Icon />
       <h3>{title}</h3>
       <p>{count}</p>
